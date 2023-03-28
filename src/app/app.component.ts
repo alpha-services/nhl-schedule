d560147204df07728eeba3c61f33795d204f3292
alpha-services <tuomas.lindell@gmail.com>
2023-03-28 19:19:47 +0400

    Feature: Added dropdown for NHL teams. Dropdown has change-listener for displaying schedule data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
 
+interface Teams{
+  teams:Array<Team>;
+}
+interface Dates{
+  dates:Array<Date>
+}
+interface Date{
+  date:string;
+}
+interface Team {
+  value: string;
+  viewValue: string;
+  name:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
-  title = 'weekly-schedule';
+  title = 'Weekly schedule';
+  constructor(private _http:HttpClient){}
+
+  teams: any = [];
+  public getTeams(){
+    return this._http.get<Teams>('https://statsapi.web.nhl.com/api/v1/teams')
+  }
+  ngOnInit(){
+
+    this.getTeams().subscribe((data) => this.teams = {
+      teams: data.teams,
+    });
+  }
+  selectedTeam = '';
+  dates:any = [];
+
+
+  public getSchedulesByTeam(teamId:string){
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append("teamId",teamId).append("startDate", "2023-01-01");
+    return this._http.get<Dates>('https://statsapi.web.nhl.com/api/v1/schedule',{params:queryParams})
+  }
+
+  onSelected(value:string): void {
+    this.selectedTeam = value;
+    this.getSchedulesByTeam(this.selectedTeam).subscribe((data)=>this.dates ={
+      dates:data.dates,
+    })
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table'
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatInputModule,
+    MatSelectModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
